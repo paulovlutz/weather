002b37208f4daa561dac1d6367280f452aff19bf
Paulo <paulovlutz@gmail.com>
2020-07-30 14:21:56 -0400

    wire up app to use google maps api

--- src/components/WeatherList.js
+++ src/components/WeatherList.js
@@ -2,6 +2,7 @@ import _ from 'lodash';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import Chart from './Chart';
+import GoogleMap from './GoogleMap';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
@@ -9,10 +10,11 @@ class WeatherList extends Component {
     const temps = _.map(cityData.list.map(weather => weather.main.temp), (temp) => temp - 273);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
+    const { lon, lat } = city.coord;
 
     return (
       <tr key={city.id}>
-        <td style={{width: '15%'}}>{city.name}</td>
+        <td><GoogleMap lon={lon} lat={lat} /></td>
         <td><Chart data={temps} color="blue" units="C" /></td>
         <td><Chart data={pressures} color="green" units="hPa" /></td>
         <td><Chart data={humidities} color="red" units="%" /></td>
--- src/reducers/reducerWeather.js
+++ src/reducers/reducerWeather.js
@@ -1,6 +1,10 @@
 import { FETCH_WEATHER } from "../actions/types";
 
 export default function(state = [], action) {
+  if (action.error) {
+    return state;
+  }
+  
   switch (action.type) {
     case FETCH_WEATHER:
       return [action.payload.data, ...state];
